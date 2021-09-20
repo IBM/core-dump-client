73072e4b4074f0c779150f1444f0a056d7c877be
Anthony Whalley <anton@venshare.com>
2021-09-20 11:45:46 +0100

    sleep container

--- nodejs/init.sh
+++ nodejs/init.sh
@@ -4,12 +4,7 @@ mc alias set storage https://$S3_REGION $S3_ACCESS_KEY $S3_SECRET
 
 mc cp storage/$S3_BUCKET_NAME/$CORE_FILE .
 
-#filename=$(basename -- "$CORE_FILE")
 dirname="${CORE_FILE%.*}"
 unzip $CORE_FILE -d $dirname
 
-exe=$EXE_LOCATION
-
-core=$dirname/$dirname.core
-
-llnode $exe -c $core
+sleep infinity
