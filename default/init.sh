dee746836584de6847406a1fea7dc9326d5a8995
Anton Whalley <anton@venshare.com>
2022-01-30 02:47:37 +0000

    feat: Setup environment for gitpods

--- default/init.sh
+++ default/init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -z ${GITPOD_INSTANCE_ID+x} ]; then 
+    echo "Running Stand Alone"; 
+else 
+    export HOME=/workspace
+    cd $HOME
+fi
+
 mc alias set storage https://$S3_REGION $S3_ACCESS_KEY $S3_SECRET
 
 mc cp storage/$S3_BUCKET_NAME/$CORE_FILE .
@@ -7,4 +14,6 @@ mc cp storage/$S3_BUCKET_NAME/$CORE_FILE .
 dirname="${CORE_FILE%.*}"
 unzip $CORE_FILE -d $dirname
 
-sleep infinity
+if [ -z ${GITPOD_INSTANCE_ID+x} ]; then 
+    sleep infinity
+fi
